e3f75c1065c9fc63217c40ce12b5dd2b5e668853
Vihung Marathe <vmarathe@deliverycube.com>
2024-10-14 10:01:06 +0100

    Ensure inbox and outbox are created before startup

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p ~/media/inbox ~/media/outbox
 mv ~/.docker/config.json ~/.docker/config.json.old; cat ~/.docker/config.json.old | sed /credsStore/d > ~/.docker/config.json; rm ~/.docker/config.json.old
 docker compose up --build
